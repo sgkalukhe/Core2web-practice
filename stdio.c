3366590db0a879d4586d0fd7e9de51bde5699f4b
sgkalukhe <shubhamkalukhe1@gmail.com>
2024-12-25 10:26:22 +0530

    Add files via upload

---
+++ stdio.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+        printf("c2w\n");
+        printf("c2w family\n");
+}
---
+++ test2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main() {
+	char X=67;
+	char ch='A';
+	int ans;
+	ans=(ch == X);
+   printf("%d\n",ans);
+   
+}
