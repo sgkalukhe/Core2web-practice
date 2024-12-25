74a5c2273653f2bef8b44f4f4625a5ac2484677a
sgkalukhe <shubhamkalukhe1@gmail.com>
2024-12-25 10:42:15 +0530

    Add files via upload

---
+++ database.c
@@ -0,0 +1,40 @@
+//lec no.3 Database//
+
+
+#include<stdio.h>
+void main ()
+{
+    //Primitive data type
+    printf("\n\nPrimitive data type\n\n");
+    int Roll_No=10;
+    float petprize=105.4500056;
+    double population=1.5984776347764;
+    char wing ='A';
+
+
+    printf("%d\n",Roll_No);
+    printf("%f\n",petprize);
+    printf("%lf\n",population);
+    printf("%c\n",wing);
+
+
+    printf("\n\nSize of data\n\n");
+    printf("%ld\n",sizeof(int));
+    printf("%ld\n",sizeof(float));
+    printf("%ld\n",sizeof(double));
+    printf("%ld\n",sizeof(char));
+    printf("%ld\n",sizeof(void));
+
+
+  /* alysis
+      changing name of inbiult function.
+
+    typedef int rahul;
+    typedef char kahna;
+
+    void main( ){
+
+             rahuulx=10;
+             kanha ch='a'; */
+
+}
